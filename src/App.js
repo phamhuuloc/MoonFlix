69065e9719b025c4a0069dbbdb922c8cde5618d3
Loc IT <loc281202@gmail.com>
2022-03-28 18:00:34 +0700

    UI Netflix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Login from "./pages/Login/Login";
 function App() {
   return (
     <>
-      <Login />
+      <Home />
     </>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,18 @@
-import {Search} from "@material-ui/icons";
-import {Notifications} from "@material-ui/icons";
-import {ArrowDropDown} from "@material-ui/icons";
-import React, {useState} from "react";
-import './navbar.scss'
+import { Search } from "@material-ui/icons";
+import { Notifications } from "@material-ui/icons";
+import { ArrowDropDown } from "@material-ui/icons";
+import React, { useState } from "react";
+import "./navbar.scss";
 const Navbar = () => {
-  const [isScrolled , setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false);
   window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true); 
-    return () => (window.onscroll = null );
-  }
-  return(
-    <div className={isScrolled ? 'navbar scrolled' : 'navbar'}>
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    // return () => (window.onscroll = null );
+  };
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
-        <div className='left'>
+        <div className="left">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
@@ -23,21 +23,24 @@ const Navbar = () => {
           <span>New and popular</span>
           <span>My list</span>
         </div>
-        <div className='right'>
-          <Search className='icon'/>
+        <div className="right">
+          <Search className="icon" />
           <span>KID</span>
-          <Notifications className='icon'/>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr0etp0uSJFItt5dtnARUT8lQumCtz6Vg0pQ&usqp=CAU" alt="" />
-          <div className='profile'>
-            <ArrowDropDown className='icon'/>
-            <ul className='options'>
-              <li className='option-item'>Setting</li>
-              <li className='option-item'>Logout</li>
+          <Notifications className="icon" />
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr0etp0uSJFItt5dtnARUT8lQumCtz6Vg0pQ&usqp=CAU"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <ul className="options">
+              <li className="option-item">Setting</li>
+              <li className="option-item">Logout</li>
             </ul>
           </div>
         </div>
       </div>
     </div>
-  )
-}
-export default Navbar; 
+  );
+};
+export default Navbar;
